22d209f34e7c74096bfbcc945f01b9ddb45a2401
kmriazuddin <mohammadriazuddin3@gmail.com>
2024-03-22 23:00:38 +0600

    dashboard can be changed

--- src/pages/dashboard/admin/Dashboard.jsx
+++ src/pages/dashboard/admin/Dashboard.jsx
@@ -19,33 +19,6 @@ const Dashboard = () => {
         }
     });
 
-    const { data: chartData = [] } = useQuery({
-        queryKey: ['orderStats'],
-        queryFn: async () => {
-            const res = await axiosSecure.get('/orderStats');
-            return res.data;
-        }
-    });
-
-    // 
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-    const RADIAN = Math.PI / 180;
-    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-        return (
-            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-                {`${(percent * 100).toFixed(0)}%`}
-            </text>
-        );
-    };
-
-    const PieChartData = chartData.map((data) => {
-        return { name: data.category, value: data.revenue }
-    })
-
     return (
         <div className='w-full md:w-[870px] mx-auto px-4'>
             <h2 className='text-2xl font-semibold my-4'>Hi, {user?.displayName}</h2>
@@ -82,51 +55,6 @@ const Dashboard = () => {
                     <div className="stat-title">Orders</div>
                     <div className="stat-value">{stats?.orders}</div>
                     <div className="stat-desc">↘︎ 90 (14%)</div>
-                </div>
-            </div>
-            {/* Chart And Graphs */}
-            <div className='mt-5 flex flex-col sm:flex-row'>
-                <div className='sm:w-1/2 w-full'>
-                    <div style={{ width: '100%', height: 300 }}>
-                        <ResponsiveContainer>
-                            <AreaChart
-                                data={chartData}
-                                margin={{
-                                    top: 10,
-                                    right: 30,
-                                    left: 0,
-                                    bottom: 0,
-                                }}
-                            >
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="category" />
-                                <YAxis />
-                                <Tooltip />
-                                <Area type="monotone" dataKey="revenue" stroke="#8884d8" fill="#8884d8" />
-                            </AreaChart>
-                        </ResponsiveContainer>
-                    </div>
-                </div>
-                <div className='sm:w-1/2 w-full'>
-                    <ResponsiveContainer width="100%" height="100%">
-                        <PieChart width={400} height={400}>
-                            <Pie
-                                data={PieChartData}
-                                cx="50%"
-                                cy="50%"
-                                labelLine={false}
-                                label={renderCustomizedLabel}
-                                outerRadius={80}
-                                fill="#8884d8"
-                                dataKey="value"
-                            >
-                                {PieChartData.map((entry, index) => (
-                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                                ))}
-                            </Pie>
-                            <Legend />
-                        </PieChart>
-                    </ResponsiveContainer>
                 </div>
             </div>
         </div>
